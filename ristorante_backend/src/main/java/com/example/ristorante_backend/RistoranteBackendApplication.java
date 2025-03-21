ac8f003c7530f26cae35bbafeb52ac7bbd01108a
Antonio Russomando <tonyrussomando@gmail.com>
2025-03-21 09:54:38 +0100

    Creazione dei progetti backend e frontend

---
+++ ristorante_backend/src/main/java/com/example/ristorante_backend/RistoranteBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.ristorante_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RistoranteBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RistoranteBackendApplication.class, args);
+	}
+
+}
